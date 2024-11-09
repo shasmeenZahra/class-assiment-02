64ea54482b198a1b5624db64512de18ce01ad414
shasmeen zahra <165050270+shasmeenZahra@users.noreply.github.com>
2024-11-09 23:31:06 +0500

    Update page.tsx

--- src/app/About/page.tsx
+++ src/app/About/page.tsx
@@ -26,25 +26,25 @@ const About: React.FC<aboutProps> = ({ profileImage = "/about.png" }) => {
           <div className={styles.content}>
             <h2>About Me</h2>
             <p>
-              I’m a web developer passionate about creating beautiful,
-              responsive websites. Hello! I’m a passionate web developer with a
+              I am a web developer passionate about creating beautiful,
+              responsive websites. Hello! I am a passionate web developer with a
               keen eye for design and a love for clean, efficient code. I thrive
               on creating intuitive digital experiences that bring ideas to
               life.
             </p>
             <p>
               With a strong foundation in web development and a creative
-              approach to problem-solving, I’m constantly exploring new
+              approach to problem-solving, I am constantly exploring new
               technologies to stay at the forefront of innovation in my field.
               My journey in tech is driven by a curiosity to learn and a
               dedication to delivering quality work.
             </p>
             <p>
               Every project is an opportunity to craft something unique and
-              meaningful. Beyond code, I’m a believer in continuous growth,
-              whether it’s through learning new skills, collaborating with
-              others, or pushing the boundaries of what’s possible in web
-              development. I’m excited to connect and work with others who share
+              meaningful. Beyond code, I am a believer in continuous growth,
+              whether its through learning new skills, collaborating with
+              others, or pushing the boundaries of what is possible in web
+              development. I am excited to connect and work with others who share
               a passion for creating exceptional digital experiences!
             </p>
           </div>
