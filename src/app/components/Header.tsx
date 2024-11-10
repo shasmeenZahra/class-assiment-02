19ed81d523b7d914e9994a0099ee22228fe1c63f
shasmeen zahra <165050270+shasmeenZahra@users.noreply.github.com>
2024-11-11 00:36:53 +0500

    Update Header.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -11,7 +11,7 @@ const Header = () => (
           <a href="/About">About</a>
         </li>
         <li>
-          <a href="/contact">Contact Us</a>
+          <a href="/contact">Contact</a>
         </li>
       </ul>
   </nav>
